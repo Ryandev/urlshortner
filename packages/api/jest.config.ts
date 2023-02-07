0ebf83f69f436da59fd67d60b03ceae8901103b3
Ryan Powell <ryangarethpowell@gmail.com>
2023-02-07 08:38:34 +0000

    Update jest config

--- packages/api/jest.config.ts
+++ packages/api/jest.config.ts
@@ -1,17 +1,17 @@
 import path = require('path');
 
-const coverage = {
+const coverage = Object.freeze({
     statements: 0,
     branches: 0,
     functions: 0,
     lines: 0,
-};
+});
 const packageName = __dirname.split(path.sep).slice(-1).join(path.sep);
 
-export default Object.freeze({
-    preset: path.join('..', '..', 'jest.preset.js'),
+export default {
     rootDir: __dirname,
-    displayName: __dirname.split('/').pop() || 'unknown',
+    preset: ['..', '..', 'jest-preset.js'].join(path.sep),
+    displayName: packageName || 'unknown',
     reporters: [
         'default',
         ['jest-junit', { outputDirectory: path.join('reports', 'test', packageName) }],
@@ -20,4 +20,4 @@ export default Object.freeze({
     coverageThreshold: {
         global: coverage,
     },
-});
+};
