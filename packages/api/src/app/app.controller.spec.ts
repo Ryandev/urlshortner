5b335ffb37cfa2d3ee62984adafa0c28b5053f83
Ryan Powell <ryangarethpowell@gmail.com>
2023-03-07 21:50:04 +0000

    Update unit tests

--- packages/api/src/app/app.controller.spec.ts
+++ packages/api/src/app/app.controller.spec.ts
@@ -1,26 +1,14 @@
-import type { TestingModule } from '@nestjs/testing';
-import { Test } from '@nestjs/testing';
-
 import AppController from './app.controller';
 import type { IAppData } from './app.interface';
 import AppService from './app.service';
 
-describe('AppController', () => {
-    let app: TestingModule | null = null;
-
-    beforeAll(async () => {
-        app = await Test.createTestingModule({
-            controllers: [AppController],
-            providers: [AppService],
-        }).compile();
-    });
-
+describe('App.Controller', () => {
     describe('getData', () => {
-        it('should return "[object Object] Welcome to api!"', () => {
-            const appController = app?.get<AppController>(AppController);
-            const actualValue: IAppData = appController?.getData() ?? { message: '' };
+        it('Validate getData() response', () => {
+            const controller = new AppController(new AppService());
+            const actualValue: IAppData = controller.getData();
             const expectedValue: IAppData = {
-                message: '[object Object] Welcome to api!',
+                message: 'AppService:{} Welcome to api!',
             };
             expect(expectedValue).toEqual(actualValue);
         });
