29648d1671ae270ec44ab120e01971a74350cd9e
Ryan Powell <git@ryanpowell.dev>
2023-05-24 15:04:48 +0100

    Update Cypress test

--- packages/frontend-e2e/cypress.config.ts
+++ packages/frontend-e2e/cypress.config.ts
@@ -1,8 +1,6 @@
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import { defineConfig } from 'cypress';
 
-console.error(`config ${JSON.stringify(nxE2EPreset(__dirname))}`); // Tmp
-
 export default defineConfig({
     e2e: nxE2EPreset(__dirname),
 });
