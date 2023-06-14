30ecd7f807676170acb25c0b9c974c2f360e55e6
Ryan Powell <git@ryanpowell.dev>
2023-06-14 09:45:08 +0100

    Remove eslint prettier rules & remove unused theme module.d.ts

--- packages/frontend/index.d.ts
+++ packages/frontend/index.d.ts
@@ -1,18 +1,4 @@
-// import './src/theme/module';
-
 import { AppThemeCustom, AppThemeOptions } from './src/theme/interface';
-// declare global {
-/* Extend MUI Theme & add additional fields */
-// declare module '@mui/material/styles/createTheme' {
-//     /* eslint-disable-next-line @typescript-eslint/no-shadow */
-//     interface BaseTheme extends AppThemeCustom, MUITheme {
-//         hello: () => 'world';
-//     }
-
-//     interface ThemeOptions extends AppThemeOptions {
-//         hello: () => 'world';
-//     }
-// }
 
 /* Extend MUI Theme & add additional fields */
 declare module '@mui/material/styles' {
@@ -20,12 +6,7 @@ declare module '@mui/material/styles' {
     interface Theme extends AppThemeCustom {}
 
     interface ThemeOptions extends AppThemeOptions {}
-
-    // type Theme = AppTheme;
-    // export function createTheme(options?: AppThemeOptions): AppTheme;
-    // declare const styled: CreateMUIStyled<AppTheme>;
 }
-// }
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 declare module '*.svg' {
--- packages/frontend/src/theme/module.d.ts
+++
@@ -1,7 +0,0 @@
-// /* Extend MUI Theme & add additional fields */
-// declare module '@mui/material/styles' {
-//     /* eslint-disable-next-line @typescript-eslint/no-shadow */
-//     type Theme = AppTheme;
-//     export function createTheme(options?: AppThemeOptions): AppTheme;
-//     // declare const styled: CreateMUIStyled<AppTheme>;
-// }
